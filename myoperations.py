01db121899e8a4d29b3247191244cf71e2107e71
David W. Farmer <farmer@aimath.org>
2015-06-10 15:15:10 -0400

    also convert exercises

--- myoperations.py
+++ myoperations.py
@@ -1,4 +1,6 @@
 
+import re
+
 import utilities
 import component
 
@@ -13,9 +15,21 @@ def mytransform(text):
 
     thetext = text
 
-    thetext = utilities.replacemacro(thetext,"\myit",0,"\\emph")
+    # replace \begin{prop}{the_label} by
+    # \begin{prop}\label{proposition:chaptername:the_label}
     thetext = utilities.replacemacro(thetext,r"\begin{prop}",1,
                          r"\begin{prop}\label{proposition:"+component.chapter_abbrev+":#1}")
+
+    # and similarly for example and exercise (yes, this can be in a loop)
+    thetext = utilities.replacemacro(thetext,r"\begin{example}",1,
+                         r"\begin{example}\label{example:"+component.chapter_abbrev+":#1}")
+    thetext = utilities.replacemacro(thetext,r"\begin{exercise}",1,
+                         r"\begin{exercise}\label{exercise:"+component.chapter_abbrev+":#1}")
+
+    # delete empty label arguments
+    thetext = re.sub(r"\\label{[a-zA-Z]+:[a-zA-Z]+:}","",thetext)
+
+    thetext = utilities.replacemacro(thetext,"\myit",0,"\\emph")
 
     return thetext
 

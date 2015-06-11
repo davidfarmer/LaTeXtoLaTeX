0d38eefe7b961380862226feb48a2dd34a23d404
David W. Farmer <farmer@aimath.org>
2015-06-11 12:09:48 -0400

    convert a directory of .tex files. needs more testing

--- ltol.py
+++ ltol.py
@@ -14,10 +14,11 @@ import myoperations
 #################################
 
 if not len(sys.argv) == 3:
-    print 'To convert a LaTeX file to a different form, do one of:'
+    print 'To convert a LaTeX file to a different form, do either:'
     print './ltol.py inputfile outputfile'
-    print 'or'
-    print './ltol.py inputdirectory outputdirectory\n'
+    print 'to convert one file, or'
+    print './ltol.py inputdirectory outputdirectory'
+    print 'to convert all the .tex files in a directory.  The outputdirectory must already exist.'
     sys.exit()
 
 component.inputname = sys.argv[1]
@@ -32,8 +33,25 @@ if os.path.isfile(component.inputname) and not os.path.isdir(component.outputnam
     component.iofilepairs.append([component.inputname,component.outputname])
     print "converting one file:",component.inputname
 
+elif os.path.isdir(component.inputname) and os.path.isdir(component.outputname):
+    inputdir = component.inputname
+    inputdir = re.sub(r"/*$","",inputdir)  # remove trailing slash
+    outputdir = component.outputname
+    outputdir = re.sub(r"/*$","",outputdir)  # remove trailing slash
+    outputdir = outputdir + "/"              # and then put it back
+    latexfiles = glob.glob(inputdir + "/*.tex")
+    for inputfilename in latexfiles:
+        outputfilename = re.sub(".*/([^/]+)", outputdir + r"\1", inputfilename)
+        if inputfilename == outputfilename:
+            print "big problem, quitting"
+        component.iofilepairs.append([inputfilename, outputfilename])
+    print latexfiles
+    print inputdir 
+    print component.iofilepairs
+#    sys.exit()
+
 else:
-    print "not proper input"
+    print "Not proper input.  Does target directory exist?"
     sys.exit()
 
 print component.iofilepairs
